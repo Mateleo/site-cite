9cd4ab32673ce2314729458b6830c2d1e1a3efa5
Théo Cohen <71221826+theo-cohen@users.noreply.github.com>
2023-09-03 12:47:29 +0200

    Update : video player
    Setup basic video player on Home age

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,18 @@
+import ReactPlayer from "react-player";
+
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
       <p>{import.meta.env.VITE_URL}</p>
+      <div>
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=GOE0Ke-niXs&ab_channel=Chtwin"
+          controls={false}
+          width="800px"
+          height="450px"
+        />
+      </div>
     </div>
-  )
+  );
 }
